65c10f01213f9f1d5c2dab1a4c9b47ad38f3a79b
Nobound <jean.ungerer.nobound@gmail.com>
2024-05-17 02:22:55 +0200

    Add 'title' field to Post model
    A new attribute 'title' has been introduced to the Post model in the front-end standalone module. This will allow post objects to contain explicit title information, improving readability and context understanding.

--- front_standalone/standalone_front/src/app/core/models/post.ts
+++ front_standalone/standalone_front/src/app/core/models/post.ts
@@ -2,6 +2,7 @@ export interface Post {
     id: number | null;
     topicId: number;
     article: string;
+    title: string;
     authorId: number;
     commentIds: number[];
     createdAt: Date;
