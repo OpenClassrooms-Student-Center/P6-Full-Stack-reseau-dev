860c52d9b739ad63e8785a519d093d42a410fc03
BENFAIALA Ibrahim <i.benfaiala@gmail.com>
2024-06-27 23:55:08 +0200

    topic model ok

--- back/src/main/java/com/openclassrooms/mddapi/model/Topic.java
+++ back/src/main/java/com/openclassrooms/mddapi/model/Topic.java
@@ -1,14 +1,10 @@
 package com.openclassrooms.mddapi.model;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -27,4 +23,6 @@ public class Topic {
 	@Column(name="description")
 	private String description;
 
+	@ManyToMany(mappedBy = "topics")
+	Set<DBUser> users;
 }
