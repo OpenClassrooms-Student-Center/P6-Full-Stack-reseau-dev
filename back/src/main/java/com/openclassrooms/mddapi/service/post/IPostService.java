acaeb0be83a84db9fae53f4b8f7a0b7e34243f44
BENFAIALA Ibrahim <i.benfaiala@gmail.com>
2024-06-27 23:55:52 +0200

    rework post service

--- back/src/main/java/com/openclassrooms/mddapi/service/post/IPostService.java
+++ back/src/main/java/com/openclassrooms/mddapi/service/post/IPostService.java
@@ -3,9 +3,7 @@ package com.openclassrooms.mddapi.service.post;
 import com.openclassrooms.mddapi.dto.DBUserDTO;
 import com.openclassrooms.mddapi.dto.PostDTO;
 import jakarta.persistence.EntityNotFoundException;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.List;
 
 public interface IPostService {
--- back/src/main/java/com/openclassrooms/mddapi/service/post/PostService.java
+++ back/src/main/java/com/openclassrooms/mddapi/service/post/PostService.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.openclassrooms.mddapi.util.DateUtils;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
 import java.sql.Timestamp;
 import java.util.List;
 import java.util.Optional;
